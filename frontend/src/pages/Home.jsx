664446005475b3a4efedd2067bbd9f3f2cd22b38
Hannah Xia <hannahxia95@gmail.com>
2026-06-04 16:06:47 -0600

    rough draft #1 for base homepage design
    Signed-off-by: Hannah Xia <hannahxia95@gmail.com>

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,14 @@ export default  function Home() {
                 <div className="petal"></div>
                 <div className="petal"></div>
 
+                <div className="sky">
+                    <div className="cloud cloud1"></div>
+                    <div className="cloud cloud2"></div>
+                    <div className="cloud cloud3"></div>
+                </div>
+
+                <div className="scene"></div>
+
                 <h1 className="title">Hi, I'm Hannah</h1>
                 <h2 className="subtitle">Software Developer and Designer</h2>
                 <p className="paragraph">
