df1c474a393f957fe51b820ff57b6e0e01024a43
Hannah Xia <hannahxia95@gmail.com>
2026-06-05 15:18:42 -0600

    write initial navbar design
    Signed-off-by: Hannah Xia <hannahxia95@gmail.com>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css"; //navbar styling
 
 export default function Navbar() {
     return (
-        <nav style={{ padding: "10px", background: "#eee" }}>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/about">About</Link> |{" "}
-            <Link to="/skills">Skills</Link> |{" "}
-            <Link to="/projects">Projects</Link> |{" "}
-            <Link to="/contact">Contact</Link>
+        <nav className="navbar">
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/skills">Skills</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </nav>
     );
 }
