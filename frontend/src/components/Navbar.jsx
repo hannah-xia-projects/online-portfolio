f2ec1674044560171dd4819ce1f14974fb755de6
Hannah Xia <hannahxia95@gmail.com>
2026-06-09 14:29:28 -0600

    initial about page content and base page
    Signed-off-by: Hannah Xia <hannahxia95@gmail.com>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import "./Navbar.css"; //navbar styling
 export default function Navbar() {
     return (
         <nav className="navbar">
+
+            <div className="logo">Hannah Xia</div>
+
             <div className="nav-links">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,35 @@
 import Navbar from "../components/Navbar";
 
-export default function About() {
-    return <h1>About Page</h1>;
-};
+export function About() {
+    return (
+        <>
+        <Navbar/>
+            <section className="about">
+                <h1>About Me</h1>
+                <p>
+                    Hello, I'm Hannah Xia, a software developer with a passion for creating. I'm currently working as a Software Developer at the AESO, where I help build real-world applications for Alberta's Electric grid.
+                </p>
+
+                <p>
+                    Here are some technologies I have been working with:
+                </p>
+                <ul className="techlist">
+                    <li><span>Java</span></li>
+                    <li><span>Java Spring</span></li>
+                    <li><span>Python</span></li>
+                    <li><span>React.js</span></li>
+                    <li><span>Node.js</span></li>
+                    <li><span>MySQL</span></li>
+
+                </ul>
+
+                <p>
+                    Outside of coding, I love the arts, learning new tech tools, and spend way too much time on gambling games. Nice to meet you!
+                </p>
+
+            </section>
+        </>
+    );
+}
+
+export default About;
