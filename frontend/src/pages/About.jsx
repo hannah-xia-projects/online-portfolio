84005be48d76d5c6dfbdb12389d8fcd428caf071
Hannah Xia <hannahxia95@gmail.com>
2026-06-12 11:22:12 -0600

    content + formatting for about page + add picture
    Signed-off-by: Hannah Xia <hannahxia95@gmail.com>

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,32 +1,41 @@
 import Navbar from "../components/Navbar";
+import HannahPic from "../assets/Hannah.jpg";
 
 export function About() {
     return (
         <>
         <Navbar/>
             <section className="about">
-                <h1>About Me</h1>
-                <p>
-                    Hello, I'm Hannah Xia, a software developer with a passion for creating. I'm currently working as a Software Developer at the AESO, where I help build real-world applications for Alberta's Electric grid.
-                </p>
+                <div className="about-container">
 
-                <p>
-                    Here are some technologies I have been working with:
-                </p>
-                <ul className="techlist">
-                    <li><span>Java</span></li>
-                    <li><span>Java Spring</span></li>
-                    <li><span>Python</span></li>
-                    <li><span>React.js</span></li>
-                    <li><span>Node.js</span></li>
-                    <li><span>MySQL</span></li>
+                    <div className="about-text">
+                        <h1>About Me</h1>
+                        <p>
+                            Hello, I'm Hannah Xia, a software developer with a passion for creating. I'm currently working as a Software Developer at the AESO, where I help build real-world applications for Alberta's Electric grid.
+                        </p>
 
-                </ul>
+                        <p>
+                            Here are some technologies I have been working with:
+                        </p>
+                        <ul className="techlist">
+                            <li><span>Java</span></li>
+                            <li><span>Java Spring</span></li>
+                            <li><span>Python</span></li>
+                            <li><span>React.js</span></li>
+                            <li><span>Node.js</span></li>
+                            <li><span>MySQL</span></li>
+                        </ul>
 
-                <p>
-                    Outside of coding, I love the arts, learning new tech tools, and spend way too much time on gambling games. Nice to meet you!
-                </p>
+                        <p>
+                            Outside of coding, I love the arts, learning new tech tools, and spend way too much time on gambling games. Nice to meet you!
+                        </p>
+                    </div>
 
+                    <div className="about-image">
+                        <img src={HannahPic} alt="Hannah" />
+                    </div>
+
+                </div>
             </section>
         </>
     );
