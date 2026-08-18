2c7d94d46751e3148f2f4897136dc80e98408843
Hannah Xia <hannahxia95@gmail.com>
2026-08-18 14:07:53 -0600

    write function of Contact.jsx
    Signed-off-by: Hannah Xia <hannahxia95@gmail.com>

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,12 +1,41 @@
 import { useEffect, useState } from "react";
 //import { handleSubmit } from "../api/contactApi";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import api from "../api/axios";
 
 import Navbar from "../components/Navbar";
 
 export default function Contact() {
+    //set up the directly message function
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await api.post("/contact", {
+            name,
+            email,
+            message,
+        })
+
+        //reset setName, etc again
+        setName("");
+        setEmail("");
+        setMessage("");
+
+        alert("Message sent!")
+    };
+
     return (
         <>
             <Navbar/>
+
+            <section className="contact">
+                <h1>Contact</h1>
+            </section>
+
         </>
     )
 };
